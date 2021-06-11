a2ac6262857d40aa3999a1f7f3c0f4ab7a270a32
komfysach <sach@getkomfy.net>
2021-06-11 15:02:42 +0200

    added reset password -completed go-auth

--- controllers/forgotController.go
+++ controllers/forgotController.go
@@ -8,6 +8,7 @@ import (
 	"festi.io/database"
 	"festi.io/models"
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Forgot(c *fiber.Ctx) error {
@@ -41,6 +42,39 @@ func Forgot(c *fiber.Ctx) error {
 	if err != nil {
 		return err
 	}
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+
+}
+
+func Reset(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	if data["password"] != data["password_confirm"] {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Passwords do not match",
+		})
+	}
+
+	var passwordReset = models.PasswordReset{}
+
+	if err := database.DB.Where("token = ?", data["token"]).Last(&passwordReset); err.Error != nil {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Invalid token!",
+		})
+	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+
+	database.DB.Model(&models.User{}).Where("email = ?", passwordReset.Email).Update("password", password)
 
 	return c.JSON(fiber.Map{
 		"message": "success",
--- routes/routes.go
+++ routes/routes.go
@@ -11,4 +11,5 @@ func Setup(app *fiber.App) {
 	app.Get("/api/user", controllers.User)
 	app.Post("/api/logout", controllers.Logout)
 	app.Post("/api/forgot", controllers.Forgot)
+	app.Post("/api/reset", controllers.Reset)
 }
