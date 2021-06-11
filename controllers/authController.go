a73604d128334281dc929f27657da9b29e3d99d0
komfysach <sach@getkomfy.net>
2021-06-11 10:57:09 +0200

    added login auth

--- controllers/authController.go
+++ controllers/authController.go
@@ -33,4 +33,34 @@ func Register(c *fiber.Ctx) error {
 	database.DB.Create(&user)
 
 	return c.JSON(user)
+
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status(404)
+		return c.JSON(fiber.Map{
+			"message": "User not found",
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(408)
+		return c.JSON(fiber.Map{
+			"message": "Incorrect password",
+		})
+	}
+
+	return c.JSON(user)
+
 }
